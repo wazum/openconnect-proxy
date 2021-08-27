e1512a775734f0dd3611c3ee4e00590e5936c90d
Jesse <jesse.kawell@semanticbits.com>
2021-08-27 10:38:56 -0400

    Add support for providing a TOTP key
    * Add ability to provide a TOTP secret key, with entrypoint generating the TOTP for each connection (rather than having to pass the actual key on startup)
    * Some fixes to the Dockerfile for broken dependencies
    * Add documentation for TOTP support

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -13,6 +13,10 @@ run () {
   elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]] && [[ ! -z "${OPENCONNECT_MFA_CODE}" ]]; then
   # Multi factor authentication (MFA)
     (echo $OPENCONNECT_PASSWORD; echo $OPENCONNECT_MFA_CODE) | openconnect -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
+  elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]] && [[ ! -z "${OPENCONNECT_TOTP_SECRET}" ]]; then
+  # Time-based One Time Password (TOTP, "Google Authenticator")
+    OPENCONNECT_TOTP=$(oathtool -b --totp "$OPENCONNECT_TOTP_SECRET")
+    echo -e "$OPENCONNECT_PASSWORD\n$OPENCONNECT_TOTP\n" | openconnect -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
   elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]]; then
   # Standard authentication
     echo $OPENCONNECT_PASSWORD | openconnect -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
